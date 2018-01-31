ebdb3a0a4c07ea8f65c2558a8834be718181ed22
tzonje <tzonje@gmail.com>
2018-01-31 15:40:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def makeWebhookResult(req):
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    zone = parameters.get("shipping-zone")[0]
 
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
