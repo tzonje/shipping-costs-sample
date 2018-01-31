0b01e8792ea1144af1afe76163ae959cfa165fe6
tzonje <tzonje@gmail.com>
2018-01-31 12:19:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def makeWebhookResult(req):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 80))
+    port = int(os.getenv('PORT', 5000))
 
     print "Starting app on port %d" % port
 
